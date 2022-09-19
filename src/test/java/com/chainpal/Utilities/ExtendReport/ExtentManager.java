9e480bfe65828e6985616c1b2f5aa2c5b2c67a60
VirenChauhan <viren.chauhan@adrixus.com>
2022-09-19 16:08:28 +0530

    Change Path
    Change path from the base calss and extent report.

--- src/test/java/com/chainpal/Utilities/ExtendReport/ExtentManager.java
+++ src/test/java/com/chainpal/Utilities/ExtendReport/ExtentManager.java
@@ -17,9 +17,9 @@ public class ExtentManager {
             DateFormat df = new SimpleDateFormat("dd_MMM_yyyy-HH_mm_ss");
             //Set HTML reporting file location
             String workingDir = System.getProperty("user.home");
-            extent = new ExtentReports("/Users/Viren/Work/Automation/old_chainpal_automation/Reports/chainpal_ReportResults_" + df.format(System.currentTimeMillis()) + ".html");
+            extent = new ExtentReports("/Users/Viren/Work/Automation/old_chainpal_automation/chainpal_automation/QA_Chainplas/Reports/chainpal_ReportResults_" + df.format(System.currentTimeMillis()) + ".html");
             extent.addSystemInfo("Environment", "Chainpal DEV Environment");
-            extent.loadConfig(new File("/Users/Viren/Work/Automation/old_chainpal_automation/src/test/java/com/chainpal/Utilities/ExtendReport/extent-config.xml"));
+            extent.loadConfig(new File("//Users/Viren/Work/Automation/old_chainpal_automation/chainpal_automation/QA_Chainplas/src/test/java/com/chainpal/Utilities/ExtendReport/extent-config.xml"));
         }
         return extent;
     }
